5774637916e582123602423dd0cf97693921ec67
rodrigobostrom <rodrigo_bst@poli.ufrj.br>
2021-11-10 15:51:57 -0300

    Some changes in power sourcer readings

--- Javascripts/PowerSource.js
+++ Javascripts/PowerSource.js
@@ -1,24 +1,57 @@
-var current = 10;
-var voltage = 10;
-var wireFeedSpeed = 1;
+var current = 0;
+var voltage = 0;
+var wireFeedSpeed = 0;
 
-var listener = new ROSLIB.Topic({
-    ros : ros,
-    name : '/power_sourcer_readings',
-    messageType : 'kuka_rsi_hw_interface/PwrSrc'
-});
+class auxPowerInfo {
+    constructor (current, voltage, wireFeedSpeed){
+        this.current = current;
+        this.voltage = voltage;
+        this.wireFeedSpeed = wireFeedSpeed;
+    }
+    setCurrent(x) {
+        this.current = x;
+    }
+    setVoltage(x) {
+        this.voltage = x;
+    }
+    setWireFeedSpeed(x) {
+        this.wireFeedSpeed = x;
+    }
+    getCurrent () {
+        return this.current;
+    }
+    getVoltage () {
+        return this.voltage;
+    }
+    getWireFeedSpeed () {
+        return this.wireFeedSpeed;
+    }
+}
 
-listener.subscribe(function(message) {
-    console.log('Received message on ' + listener.name + ': ' + 'Corrente: ' + message.current + 'Tensão' + message.voltage);
-    //document.getElementById('power_sources').innerHTML += '<h4>'+ 'Corrente: ' + message.current +'</h4>'
-    //document.getElementById('power_sources').innerHTML += '<h4>'+ 'Tensão: ' + message.voltage +'</h4>'
-});
+function createPowerSourcerSubscriber(){
+
+    var powerInfo = new auxPowerInfo(current, voltage, wireFeedSpeed);
+
+    var listener = new ROSLIB.Topic({
+        ros : ros,
+        name : '/power_sourcer_readings',
+        messageType : 'kuka_rsi_hw_interface/PwrSrc'
+    });
+
+    listener.subscribe(function (message){
+            //console.log("Teste");
+            powerInfo.setCurrent(message.current);
+            powerInfo.setVoltage(message.voltage);
+            powerInfo.setWireFeedSpeed(message.wire_feed_speed);
+            console.log("Corrente: " + powerInfo.current + " Voltagem: " + powerInfo.voltage);
+    });
+}
 
 function createTable() {
-    var controlTable = document.getElementById("power_sources");
 
-    //real_current = message.current;
-    //real_voltage = message.voltage;
+    var PowerInfo = new auxPowerInfo(current, voltage, wireFeedSpeed);
+
+    var controlTable = document.getElementById("power_sourcer");
 
     for (var numberRows = 0; numberRows < 3; numberRows++) {
         var row = controlTable.insertRow(numberRows);
@@ -45,8 +78,8 @@ function createTable() {
                             NameCell.innerHTML = rowName.bold();
                             break;
                         case 1:
-                            ValueCell.innerHTML = current;
-
+                            var auxPowerInfoFunc = powerInfo.getCurrent();
+                            ValueCell.innerHTML = auxPowerInfoFunc();
                             ValueCell.tabIndex = "1";
                             break;
                     }
@@ -62,9 +95,9 @@ function createTable() {
                             NameCell.innerHTML = rowName.bold();
                             break;
                         case 1:
-                            ValueCell.innerHTML = voltage;
-
-                            ValueCell.tabIndex = "2";
+                            var auxPowerInfoFunc = powerInfo.getCurrent();
+                            ValueCell.innerHTML = auxPowerInfoFunc();
+                            ValueCell.tabIndex = "1";
                             break;
                     }
                     break;
@@ -79,9 +112,9 @@ function createTable() {
                             NameCell.innerHTML = rowName.bold();
                             break;
                         case 1:
-                            ValueCell.innerHTML = wireFeedSpeed;
-
-                            ValueCell.tabIndex = "3";
+                            var auxPowerInfoFunc = powerInfo.getCurrent();
+                            ValueCell.innerHTML = auxPowerInfoFunc();
+                            ValueCell.tabIndex = "1";
                             break;
                     }
                     break;
@@ -89,3 +122,37 @@ function createTable() {
         }
     }
 }
+
+function updatePowerSourcerReadings()
+{
+    var updatePwrSrc = document.getElementById("power_sourcer");
+
+    for (var i = 1; i != 9; i++)
+    {
+        var auxPowerInfoFunc = auxPowerInfo[0].getCurrent();
+        var current = Math.abs(auxPowerInfoFunc());
+        
+        if(current > 3000 && current < 7000)
+        {
+            updatePwrSrc.rows[i].cells.item(1).style.backgroundColor = 'yellow';
+            updatePwrSrc.rows[i].cells.item(1).style.color = 'black';
+            updatePwrSrc.rows[i].cells.item(1).innerHTML = (current.toString()).bold();
+        }
+        else if (current > 7000)
+        {
+            updatePwrSrc.rows[i].cells.item(1).style.backgroundColor = 'red';
+            updatePwrSrc.rows[i].cells.item(1).style.color = 'black';
+            updatePwrSrc.rows[i].cells.item(1).innerHTML = (current.toString()).bold();
+        }
+        else
+        {
+            updatePwrSrc.rows[i].cells.item(1).style.backgroundColor = 'white';
+            updatePwrSrc.rows[i].cells.item(1).style.color = 'black';
+            updatePwrSrc.rows[i].cells.item(1).innerHTML = current;
+        }
+    }
+}
+
+var dOut2 = [false, false, false];
+var dOut3 = [false, false, false];
+var dOut = [dOut2, dOut3];
